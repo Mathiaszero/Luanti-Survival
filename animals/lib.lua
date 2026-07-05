968b01415f6f30e963242f8bb19e8dd0cc3f8052
Mathiaszero <55817193+Mathiaszero@users.noreply.github.com>
2026-07-05 18:36:34 -0400

    fixed damage to player when attacking mobs

--- animals/lib.lua
+++ animals/lib.lua
@@ -1,16 +1,15 @@
 function animals.retaliate(puncher, damage)
-    local max_dmg = damage
     local inv = puncher:get_inventory()
     if inv:contains_item("main", "animals:leather_armor") then
-        max_dmg = max_dmg - 1
-    elseif inv:contains_item("main", "animals: reinforced_leather_armor") then
-        max_dmg = max_dmg - 2
+        damage = damage - 1
+    elseif inv:contains_item("main", "animals:reinforced_leather_armor") then
+        damage = damage - 2
     end
-    if max_dmg < 1 then
-        max_dmg = 1
+    if damage < 1 then
+        damage = 1
     end
-    dmg = math.random(1, max_dmg)
-    return dmg
+    damage = math.random(1, damage)
+    return damage
 end
 
 core.register_craftitem("animals:leather", {
